8587280ab45b25b08c498119ca4f22989ac4b6cd
ljieun1228 <ljieun1228@naver.com>
2018-12-11 16:42:53 +0900

    2018-12-11 16:42

---
+++ arithmetic/Sequence.java
@@ -0,0 +1,22 @@
+package arithmetic;
+
+/*등차수열 : 연속하는 두 항의 차이가 모두 일정한 수열
+ * 공차(common difference) : 공통적으로 나타나는 차 
+ */
+//2+8+14+20+26=70
+public class Sequence {
+	public static void main(String[] args) {
+		String res = "";
+		int sum = 0;
+		for(int i=0;i<5;i++){
+			if(i!=4){
+				System.out.print(2+6*i+"+");
+			}else
+				System.out.print(2+6*i+"=");
+			}
+			for(int i=0;i<5;i++){
+			sum += 2+6*i;
+		}
+			System.out.print(sum);
+	}
+}
